6ea3f64d305934c39bbb659147a44740e10d22a4
Eduardo Mendes <eduardowilliangm@gmail.com>
2022-01-26 19:47:44 -0300

    Added commentaries to the HashMapDemo class

--- src/main/java/set/map/HashMapDemo.java
+++ src/main/java/set/map/HashMapDemo.java
@@ -7,23 +7,29 @@ import java.util.Set;
 public class HashMapDemo {
 
     public static void main(String[] args) {
+        // Cria um HashMap
         HashMap<String, Double> hashMap = new HashMap<>();
 
+        // Insere elementos no HashMap
+        // Usa put() para inserir entradas
         hashMap.put("John Doe", 3434.34);
         hashMap.put("Tom Smith", 123.22);
         hashMap.put("Jane Baker", 1378.00);
         hashMap.put("Todd Hall", 99.22);
         hashMap.put("Ralph Smith", -19.08);
 
-        Set<Map.Entry<String, Double>> set = hashMap.entrySet();
+        // Obtém um conjunto das entradas
+        Set<Map.Entry<String, Double>> set = hashMap.entrySet();//Obtém uma visão de conjunto das entradas
 
+        // Exibe o conjunto
         for (Map.Entry<String, Double> mapEntry : set) {
-            System.out.print(mapEntry.getKey() + ": ");
-            System.out.println(mapEntry.getValue());
+            System.out.print(mapEntry.getKey() + ": ");// Exibe as chaves
+            System.out.println(mapEntry.getValue());// Exibe os valores
         }
 
         System.out.println();
 
+        //Deposita 1000 na conta de John Doe
         double balance = hashMap.get("John Doe");
         hashMap.put("John Doe", balance + 1000);
 
